6437687df73b21a53280e53f0d8205d094f29744
habiibn <habibnurkholis25@gmail.com>
2022-12-04 04:48:55 +0700

    responsive layout

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+    myFunction()
+};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    if (window.pageYOffset >= sticky) {
+        navbar
+            .classList
+            .add("sticky")
+    } else {
+        navbar
+            .classList
+            .remove("sticky");
+    }
   }
-  else {
-    navbar.classList.remove("sticky");
-  }
-}
